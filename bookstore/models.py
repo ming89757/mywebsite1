904d933323aec59a6e37871f9e0a5d06e6829026
GMZ <1023125251@qq.com>
2020-12-11 21:42:55 +0800

    添加作者表

--- bookstore/models.py
+++ bookstore/models.py
@@ -4,5 +4,20 @@ from django.db import models
 
 
 class Book(models.Model):
-    title = models.CharField('book_name', max_length=100)
-    pub = models.CharField('pub', max_length=200, null=True)
+    title = models.CharField('书名', max_length=100, unique=True)
+    pub = models.CharField('出版社', max_length=200)
+    price = models.IntegerField('图书价格')
+    market_price = models.IntegerField('图书零售价')
+
+    def __str__(self):
+        return self.title
+
+
+class Author(models.Model):
+    name = models.CharField('姓名', max_length=100,  unique=True, db_index=True)
+    age = models.IntegerField('年龄', default=1)
+    email = models.EmailField('邮箱', null=True)
+
+    def __str__(self):
+        return self.name
+
