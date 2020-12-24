494d44f354fb495bb2b7e825a03840ca0ace676f
ming89757 <63713077+ming89757@users.noreply.github.com>
2020-12-24 18:51:07 +0800

    Update settings.py
    change

--- mywebsite1/settings.py
+++ mywebsite1/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'webdb',
         'USER': 'gmz',
-        'PASSWORD': 'mingzhu1',
+        'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
